d86cad2407cdec3911a6a93b85ca298ba9cdf885
Simon Dellicour <simondellicour@gmail.com>
2019-07-07 22:30:19 +0200

    initial commit

---
+++ macOS/R/circuitScape.R
@@ -0,0 +1,115 @@
+circuitScape <-
+function (envVariable, envVariableName, resistance = TRUE, avgResistance = TRUE, 
+    fourCells = FALSE, fromCoor, toCoor, OS = "Unix", prefix = "", 
+    ID = "", nberOfCores_CS = 1) 
+{
+    mat = matrix(nrow = dim(as.matrix(fromCoor))[1], ncol = 1)
+    folder = paste(prefix, "_CStemp_", ID, sep = "")
+    if (folder %in% dir(getwd()) == FALSE) 
+        dir.create(file.path(getwd(), folder))
+    cs.text = file(paste(folder, "CS_script.sh", sep = "/"))
+    line1 = "from circuitscape import Compute"
+    line2 = paste("cs = Compute('", folder, "/CS_temporary.ini', 'Screen')", 
+        sep = "")
+    line3 = "result = cs.compute()"
+    writeLines(c(line1, line2, line3), cs.text)
+    close(cs.text)
+    data(cs)
+    index = which(grepl("point_file = ", cs.ini))
+    cs.ini[index] = paste("point_file = ", getwd(), paste("/", 
+        folder, "/focal_points_temp.txt", sep = ""), sep = "")
+    index = which(grepl("output_file = ", cs.ini))
+    cs.ini[index] = paste("output_file = ", getwd(), paste("/", 
+        folder, "/raster_file_temp.txt", sep = ""), sep = "")
+    index = which(grepl("log_file = XXXX", cs.ini))
+    cs.ini[index] = paste("log_file = ", getwd(), paste("/", 
+        folder, "/raster_file_temp.log", sep = ""), sep = "")
+    index = which(grepl("included_pairs_file = XXXX", cs.ini))
+    cs.ini[index] = paste("included_pairs_file = ", getwd(), 
+        paste("/", folder, "/pairs_to_include.txt", sep = ""), 
+        sep = "")
+    index = which(grepl("max_parallel = ", cs.ini))
+    cs.ini[index] = paste("max_parallel = ", nberOfCores_CS, 
+        sep = "")
+    index = which(grepl("habitat_file = ", cs.ini))
+    if (grepl(".asc", envVariableName) | grepl(".tif", envVariableName) | 
+        grepl(".gri", envVariableName)) {
+        cs.ini[index] = paste("habitat_file = ", getwd(), "/", 
+            envVariableName, sep = "")
+    }
+    else {
+        cs.ini[index] = paste("habitat_file = ", getwd(), "/", 
+            envVariableName, ".asc", sep = "")
+    }
+    if (OS == "Windows") 
+        cs.ini = gsub("/", "\\", cs.ini, fixed = T)
+    index = which(grepl("habitat_map_is_resistances = ", cs.ini))
+    if (resistance == TRUE) {
+        cs.ini[index] = "habitat_map_is_resistances = True"
+    }
+    else {
+        cs.ini[index] = "habitat_map_is_resistances = False"
+    }
+    index = which(grepl("connect_using_avg_resistances = ", cs.ini))
+    if (avgResistance == TRUE) {
+        cs.ini[index] = "connect_using_avg_resistances = True"
+    }
+    else {
+        cs.ini[index] = "connect_using_avg_resistances = False"
+    }
+    index = which(grepl("connect_four_neighbors_only = ", cs.ini))
+    if (fourCells == TRUE) {
+        cs.ini[index] = "connect_four_neighbors_only = True"
+    }
+    else {
+        cs.ini[index] = "connect_four_neighbors_only = False"
+    }
+    cs.text = file(paste(folder, "CS_temporary.ini", sep = "/"))
+    writeLines(cs.ini, cs.text)
+    close(cs.text)
+    focal_points = paste(folder, "focal_points_temp.txt", sep = "/")
+    lines1 = cbind(1:length(fromCoor[, 1]), fromCoor)
+    lines2 = cbind((length(fromCoor[, 1]) + 1):(length(fromCoor[, 
+        1]) + length(toCoor[, 1])), toCoor)
+    lines = rbind(lines1, lines2)
+    write.table(lines, focal_points, row.names = F, col.names = F)
+    pairs_to_include = paste(folder, "pairs_to_include.txt", 
+        sep = "/")
+    tab = matrix(nrow = dim(mat)[1], ncol = 2)
+    colnames(tab) = c("mode", "include")
+    tab[, 1] = 1:dim(mat)[1]
+    tab[, 2] = (dim(mat)[1] + 1):(2 * dim(mat)[1])
+    write.table(tab, pairs_to_include, row.names = F, col.names = T, 
+        sep = "\t", quote = F)
+    if (OS == "Unix") {
+        system(paste("python", paste(folder, "CS_script.sh", 
+            sep = "/"), sep = " "), ignore.stdout = T, ignore.stderr = T)
+    }
+    if (OS == "Windows") {
+        system(paste("\"C:\\Program Files\\Circuitscape\\cs_run.exe\"", 
+            paste(folder, "CS_temporary.ini", sep = "/"), sep = " "), 
+            ignore.stdout = T, ignore.stderr = T)
+    }
+    tab = read.table(paste(folder, "/raster_file_temp_resistances.txt", 
+        sep = ""), header = F)
+    tab = tab[2:dim(tab)[1], 2:dim(tab)[2]]
+    sameCoordinates = FALSE
+    if (sum(fromCoor - toCoor) == 0) 
+        sameCoordinates = TRUE
+    if (sameCoordinates == FALSE) {
+        for (i in 1:length(fromCoor[, 1])) {
+            mat[i] = tab[i, (i + length(fromCoor[, 1]))]
+        }
+    }
+    else {
+        mat = matrix(nrow = dim(as.matrix(fromCoor))[1], ncol = dim(as.matrix(fromCoor))[1])
+        mat = tab[1:dim(fromCoor)[1], 1:dim(fromCoor)[1]]
+    }
+    if (OS == "Unix") {
+        system(paste("rm -rf ", folder, sep = ""))
+    }
+    if (OS == "Windows") {
+        unlink(folder, recursive = T, force = T)
+    }
+    return(mat)
+}
---
+++ macOS/R/treeExtractions.R
@@ -0,0 +1,205 @@
+treeExtractions <-
+function (localTreesDirectory, allTrees, burnIn, randomSampling, 
+    nberOfTreesToSample, mostRecentSamplingDatum, coordinateAttributeName, 
+    nberOfCores = 1) 
+{
+    registerDoMC(cores = nberOfCores)
+    dir.create(file.path(localTreesDirectory), showWarnings = F)
+    MCCtree = FALSE
+    newTreeList = list()
+    count = 1
+    for (i in 1:length(allTrees)) {
+        if (substring(allTrees[[i]], 1, 4) == "tree") {
+            newTreeList[[count]] = allTrees[[i]]
+            count = count + 1
+        }
+    }
+    nberOfTrees = length(newTreeList)
+    newTreeListMinusBurnIn = newTreeList[(burnIn + 1):nberOfTrees]
+    regularSampledTrees = newTreeList[nberOfTreesToSample]
+    if (randomSampling == TRUE) {
+        sampledNewTreeListMinusBurnIn = sample(newTreeListMinusBurnIn, 
+            nberOfTreesToSample)
+    }
+    else {
+        intervale = floor(length(newTreeListMinusBurnIn)/nberOfTreesToSample)
+        index = 0
+        for (i in 1:nberOfTreesToSample) {
+            index = index + intervale
+            regularSampledTrees[i] = newTreeListMinusBurnIn[index]
+        }
+        sampledNewTreeListMinusBurnIn = regularSampledTrees
+    }
+    buffer = list()
+    buffer = foreach(i = 1:length(sampledNewTreeListMinusBurnIn)) %dopar% 
+        {
+            extractionsID = runif(1, 0, 1)
+            properTreeName = paste("ProperTree_", extractionsID, 
+                ".tree", sep = "")
+            tree = sampledNewTreeListMinusBurnIn[i]
+            if (MCCtree == FALSE) {
+                treeID = gsub(pattern = "\\[.+\\];", replacement = "", 
+                  tree)
+                treeID = gsub(pattern = " ", replacement = "", 
+                  treeID)
+                treeID = unlist(strsplit(treeID, "_"))[2]
+                cat("Exctracting information from sampled tree n\302\260", 
+                  treeID, "\n", sep = "")
+            }
+            else {
+                treeID = "MCC_tree"
+            }
+            tab = gsub(pattern = "tree.+\\[&R\\]", replacement = "", 
+                tree)
+            tab = gsub(pattern = "\\}\\]:\\[&rate=", replacement = "\\},rate=", 
+                tab)
+            tab = gsub(pattern = "\\}\\]:\\[&+[[:alnum:]]+[[:punct:]]+rate=", 
+                replacement = "\\},rate=", tab)
+            tab = gsub("\\[&CO", "\\&&CO", tab)
+            tab = unlist(strsplit(tab, "\\["))[-1]
+            tab = gsub("&", "", tab)
+            tab = gsub("\\}.+$", "", tab)
+            tab = gsub("\\{", "", tab)
+            tab = gsub(paste(".*", coordinateAttributeName, "=", 
+                sep = ""), "location1=", tab)
+            tab = gsub(",", ",location2=", tab)
+            foo = function(x) {
+                x = unlist(strsplit(x, ","))
+                x
+            }
+            tab = lapply(tab, foo)
+            tab_transit = data.frame()
+            colNames = unique(gsub("=.+$", "", unlist(tab)))
+            colNames = gsub(" ", "", colNames)
+            for (j in seq(tab)) {
+                for (k in seq(colNames)) {
+                  ind = grep(paste("^", colNames[k], "=", sep = ""), 
+                    tab[[j]])
+                  if (length(ind) > 0) {
+                    v = as.numeric(gsub(paste(colNames[k], "=", 
+                      sep = ""), "", tab[[j]][ind]))
+                    tab_transit[j, k] = v
+                  }
+                }
+            }
+            tab = tab_transit
+            colnames(tab) = colNames
+            all = which(!is.na(tab[, 1]))
+            tree = gsub("\\[[^]]*\\]", "", tree)
+            tree = gsub("tree STATE_.+[[:space:]]+=[[:space:]]+", 
+                "", tree)
+            write(tree, file = properTreeName)
+            tree1 = unlist(strsplit(tree, "\\("))
+            tree1 = unlist(strsplit(tree1, ","))
+            tree1 = unlist(strsplit(tree1, "\\)"))
+            tree2 = unlist(strsplit(tree, ":"))[-1]
+            tree2 = gsub("[( | ) | ;]", "", tree2)
+            tree2 = strsplit(tree2, ",")
+            foo = function(x) x = head(x, 1)
+            tree2 = unlist(lapply(tree2, foo))
+            tree2 = paste("", tree2, sep = ":")
+            tree2 = c(tree2, ";")
+            allStats = vector(mode = "list", length = dim(tab)[2])
+            for (j in seq(allStats)) {
+                tree3 = tree
+                val = tab[, j]
+                tree4 = paste(val, tree2, sep = "")
+                for (k in all) {
+                  tree3 = gsub(paste("\\(", tree1[k], sep = ""), 
+                    paste("\\(", tree4[k], sep = ""), tree3)
+                  tree3 = gsub(paste("\\)", tree1[k], sep = ""), 
+                    paste("\\)", tree4[k], sep = ""), tree3)
+                  tree3 = gsub(paste(",", tree1[k], sep = ""), 
+                    paste(",", tree4[k], sep = ""), tree3)
+                  tree5 = read.tree(text = tree3)
+                  tree6 = c(tree5$tip.label, tree5$node.label)
+                  tree6[tree6 == "NA"] = 9999
+                  tree6 = as.numeric(tree6)
+                  tree6[tree6 == 9999] = NA
+                  allStats[[j]] = tree6
+                  names(allStats)[j] = colnames(tab)[j]
+                }
+            }
+            mat = matrix(unlist(allStats), ncol = length(allStats), 
+                byrow = F)
+            tree = read.tree(properTreeName)
+            treeEdges = tree$edge
+            treeEdgesLengths = tree$edge.length
+            treeEdgesLengths = as.matrix(treeEdgesLengths, nrows = (b[1]), 
+                ncol = 1)
+            treeEdgesLengths = cbind(treeEdges, treeEdgesLengths)
+            mat = cbind(mat, c(1:length(mat[, 1])))
+            mat = cbind(mat, c(1:length(mat[, 1])))
+            colnames(mat) = c("latitude", "longitude", "node1", 
+                "node2")
+            colnames(treeEdgesLengths) = c("node1", "node2", 
+                "length")
+            mat_transit = merge(treeEdgesLengths, mat, "node2")
+            colnames(mat_transit) = c("node2", "node1", "length", 
+                "endStates", "endRate", "x")
+            mat_transit = merge(mat_transit, mat, "node1")
+            colnames(mat_transit) = c("node1", "node2", "length", 
+                "endLat", "endLon", "x", "startLat", "startLon", 
+                "y")
+            mat_transit$x = NULL
+            mat_transit$y = NULL
+            mat = mat_transit[, c(1, 2, 3, 6, 7, 4, 5)]
+            l = length(mat[, 1])
+            if (l > 0) {
+                ll = matrix(1:l, nrow = l, ncol = l)
+                ll[] = 0
+                for (j in 1:l) {
+                  subMat = mat[j, 2]
+                  subMat = subset(mat, mat[, 2] == subMat)
+                  ll[j, 1] = subMat[, 3]
+                  subMat = subMat[1, 1]
+                  subMat1 = subset(mat, mat[, 2] == subMat)
+                  for (k in 1:l) {
+                    if (nrow(subMat1) > 0) {
+                      ll[j, k + 1] = subMat1[, 3]
+                      subMat2 = subMat1[1, 1]
+                      subMat1 = subset(mat, mat[, 2] == subMat2)
+                    }
+                  }
+                }
+                endNodeL = rowSums(ll)
+                mat = cbind(mat, endNodeL)
+                startNodeL = matrix(1:l, nrow = l, ncol = 1)
+                startNodeL[] = 0
+                for (j in 1:l) {
+                  r = mat[j, 1]
+                  s = subset(mat, mat[, 2] == r)
+                  for (k in 1:l) {
+                    if (nrow(s) > 0) {
+                      startNodeL[j, 1] = s[, 8]
+                    }
+                  }
+                }
+                mat = cbind(mat, startNodeL)
+                maxEndLIndice = which.max(mat[, "endNodeL"])
+                maxEndL = mat[maxEndLIndice, "endNodeL"]
+                endYear = matrix(mat[, "endNodeL"] - maxEndL)
+                endYear = matrix(mostRecentSamplingDatum + (endYear[, 
+                  1]))
+                startYear = matrix(mat[, "startNodeL"] - maxEndL)
+                startYear = matrix(mostRecentSamplingDatum + 
+                  (startYear[, 1]))
+                mat = cbind(mat, startYear, endYear)
+                treeIDs = matrix(0, nrow = dim(mat)[1], ncol = 1)
+                colnames(treeIDs) = c("treeID")
+                for (j in 1:dim(mat)[1]) {
+                  treeIDs[j] = treeID
+                }
+                mat = cbind(mat, treeIDs)
+                file.remove(properTreeName)
+                file = as.character(paste(localTreesDirectory, 
+                  "/TreeExtractions_", i, ".csv", sep = ""))
+                write.csv(mat, file, row.names = F, quote = F)
+            }
+            else {
+                file.remove(properTreeName)
+                print(paste("A problem occured with tree extraction ", 
+                  i, sep = ""))
+            }
+        }
+}
